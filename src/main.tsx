92399c267a700c11ed161beb4a0ae7041fcd4cbe
mbugiel656 <mbugiel656@gmail.com>
2023-01-07 16:26:03 +0100

    Project configuration, installing packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+import Homepage from './pages/Homepage'
+
 import './index.css'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Homepage = () => {
+    return (
+        <div className="">
+            <h1 className="text-3xl font-bold">eCommerce</h1>
+        </div>
+    )
+}
+
+export default Homepage
