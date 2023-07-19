38907c9e412aad2a55a8ac5694a212814a30318c
mbugiel656 <mbugiel656@gmail.com>
2023-07-19 12:06:44 +0200

    Create axios instance and and request interceptor

---
+++ src/api.tsx
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { 
+    AxiosInstance, 
+    AxiosResponse 
+} from 'axios'
+
+
+const api: AxiosInstance = axios.create({
+    baseURL: 'http://localhost:3000/api/'
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('authToken')
+        if (token) {
+            api.defaults.headers.common['Authorization'] = `Token ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api
