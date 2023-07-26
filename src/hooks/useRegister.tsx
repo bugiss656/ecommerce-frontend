d4f954f9819774a5a44790f7b1f8c0968a56e401
mbugiel656 <mbugiel656@gmail.com>
2023-07-26 14:11:53 +0200

    Changes in useRegister hook

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,5 +1,6 @@
 import api from "../api"
 import { useState } from "react"
+import { AxiosError } from "axios"
 
 
 type RegistrationData = (
@@ -7,22 +8,35 @@ type RegistrationData = (
     password: string,
     firstName: string,
     lastName: string,
-    phone: string
+    // phone: string
 ) => Promise<void>
 
 
 export const useRegister = () => {
     const [isRegistrationSuccess, setIsRegistrationSuccess] = useState(false)
+    const [error, setError] = useState('')
 
-    const register: RegistrationData = async (email, password, firstName, lastName, phone) => {
+    const register: RegistrationData = async (email, password, firstName, lastName) => {
         try {
-            await api.post('register/', { email, password, firstName, lastName, phone })
+            await api.post('customers/register/', { 
+                email: email, 
+                password: password, 
+                first_name: firstName, 
+                last_name: lastName 
+            }, {
+                headers: {
+                    'Content-Type': 'application/json' 
+                }
+            })
             setIsRegistrationSuccess(true)
-        } catch (error) {
-            console.error('Registration failed', error)
-            setIsRegistrationSuccess(false)
+        } catch (error: any | AxiosError) {
+            if (error.response.data.email && error.response.status === 400) {
+                setIsRegistrationSuccess(false)
+                console.error('Registration failed', error)
+                setError('Ten adres email jest już zajęty. Podaj inny adres lub odzyskaj hasło.')
+            }
         }
     }
 
-    return { register, isRegistrationSuccess }
+    return { register, isRegistrationSuccess, error }
 }
