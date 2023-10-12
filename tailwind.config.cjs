74be6c61c8e49856d3eb175a9eacfc76ba7c0911
mbugiel656 <mbugiel656@gmail.com>
2023-10-12 13:05:00 +0200

    Add custom shadow rules in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'primary': 'rgba(0, 0, 0, 0.08) 0px 2px 4px 0px, rgba(0, 0, 0, 0.08) 0px 0px 2px 1px',
+        'secondary': 'rgba(0, 0, 0, 0.16) 0px 4px 8px 0px, rgba(0, 0, 0, 0.08) 0px 0px 2px 1px'
+      }
+    },
     container: {
       center: true,
     }
