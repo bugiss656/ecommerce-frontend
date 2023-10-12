63a65ae46bb243a4165d873d00209c3392d6ec6b
mbugiel656 <mbugiel656@gmail.com>
2023-10-12 13:03:35 +0200

    Refactoring Account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,3 +1,5 @@
+import { lists } from '../mockdata'
+
 import { BsCardList } from 'react-icons/bs'
 import { FaRegAddressCard } from 'react-icons/fa'
 import { AiOutlineHeart } from 'react-icons/ai'
@@ -6,8 +8,40 @@ import { BsCardImage } from 'react-icons/bs'
 
 import { AccountMenu, AccountMenuItem } from '../components/AccountMenu/AccountMenu'
 import { OrderList, OrderListItem } from '../components/OrderList/OrderList'
-import { ShoppingList, ShoppingListItem } from '../components/ShoppingList/ShoppingList'
+import { ShoppingListCard } from '../components/ShoppingList/ShoppingList'
 
+import { Outlet } from 'react-router-dom'
+
+
+export const AccountMain = () => {
+    return (
+        <>
+            <div className="mb-8">
+                <div className="flex flex-row justify-between items-center mb-6">
+                    <h1 className="text-2xl">Zamówienia</h1>
+                    <a href="" className="px-3 py-1 rounded hover:bg-slate-100">Zobacz więcej</a>
+                </div>
+                <OrderList>
+                    <OrderListItem icon={<BsCardImage />} />
+                </OrderList>
+            </div>
+            <div className="mb-8">
+                <div className="flex flex-row justify-between items-center mb-6">
+                    <h1 className="text-2xl">Listy zakupowe</h1>
+                    <a href="" className="px-3 py-1 rounded hover:bg-slate-100">Zobacz więcej</a>
+                </div>
+                {lists.map((list: any) =>
+                    <ShoppingListCard
+                        name={list.name}
+                        lastUpdate={list.lastUpdate}
+                        products={list.products}
+                        price={list.price}
+                    />
+                )}
+            </div>
+        </>
+    )
+}
 
 const Account = () => {
     return (
@@ -20,16 +54,7 @@ const Account = () => {
                     <AccountMenuItem icon={<FiSettings />} text='Ustawienia konta' />
                 </AccountMenu>
                 <div className="w-4/5 border-l-[1px] px-4">
-                    <div className="mb-8">
-                        <OrderList>
-                            <OrderListItem icon={<BsCardImage />} />
-                        </OrderList>
-                    </div>
-                    <div className="mb-8">
-                        <ShoppingList>
-                            <ShoppingListItem />
-                        </ShoppingList>
-                    </div>
+                    <Outlet />
                 </div>
             </div>
         </div>
