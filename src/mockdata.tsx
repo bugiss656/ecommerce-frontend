75875256a5735a2302c024aa7b53cf53ca7519c9
mbugiel656 <mbugiel656@gmail.com>
2023-10-12 13:05:47 +0200

    Create mock data for components

---
+++ src/mockdata.tsx
@@ -0,0 +1,80 @@
+import imgSrc from './assets/product.jpg'
+
+
+export const lists = [
+    {
+        name: 'Lista zakupowa 1',
+        lastUpdate: '11.11.2023',
+        products: [
+            {
+                name: 'Produkt 1',
+                price: '10,99 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+            {
+                name: 'Produkt 2',
+                price: '9,99 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+            {
+                name: 'Produkt 3',
+                price: '20 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+        ],
+        price: '100 zł'
+    },
+    {
+        name: 'Lista zakupowa 2',
+        lastUpdate: '11.11.2023',
+        products: [
+            {
+                name: 'Produkt 4',
+                price: '30 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+            {
+                name: 'Produkt 5',
+                price: '120 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+            {
+                name: 'Produkt 6',
+                price: '129,99 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+        ],
+        price: '200 zł'
+    },
+    {
+        name: 'Lista zakupowa 3',
+        lastUpdate: '11.11.2023',
+        products: [
+            {
+                name: 'Produkt 7',
+                price: '16 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+            {
+                name: 'Produkt 8',
+                price: '100 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+            {
+                name: 'Produkt 9',
+                price: '60 zł',
+                quantity: 2,
+                imageSrc: imgSrc
+            },
+        ],
+        price: '300 zł'
+    },
+]
