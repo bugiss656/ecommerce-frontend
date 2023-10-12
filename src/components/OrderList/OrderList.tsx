23b28b2aab608fe82078498f7f5de41ed1bd2134
mbugiel656 <mbugiel656@gmail.com>
2023-10-12 12:59:42 +0200

    Refactoring and changes in components: OrderList, ShoppingList

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -11,11 +11,11 @@ type OrderListProps = {
 
 export const OrderListItem = ({ icon }: OrderListItemProps) => {
     return (
-        <a href="" className="flex flex-row shadow-md rounded-lg mb-4">
+        <a href="" className="flex flex-row shadow-primary hover:shadow-secondary rounded-lg mb-4">
             <div className="w-1/4 p-6 bg-gray-100">
-                <h1 className="text-lg font-medium">Zakończone</h1>
+                <h1 className="text-lg font-medium mb-2">Zakończone</h1>
                 <p>01 luty 2023</p>
-                <p>199,99 zł</p>
+                <p className="font-medium mt-2">199,99 zł</p>
             </div>
             <div className="flex items-center justify-center w-3/4">
                 {icon}
@@ -27,14 +27,8 @@ export const OrderListItem = ({ icon }: OrderListItemProps) => {
 
 export const OrderList = ({ children }: OrderListProps) => {
     return (
-        <>
-            <div className="flex flex-row justify-between items-center mb-6">
-                <h1 className="text-2xl">Zamówienia</h1>
-                <a href="" className="px-3 py-1 rounded hover:bg-slate-100">Zobacz więcej</a>
-            </div>
-            <div className="w-full">
-                {children}
-            </div>
-        </>
+        <div className="w-full">
+            {children}
+        </div>
     )
 }
--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -1,60 +1,84 @@
-import { ReactElement } from "react"
-import { IconContext } from "react-icons"
-
-import { BsCardImage } from 'react-icons/bs'
 
 
-type ShoppingListItemProps = {
-    icon?: ReactElement
+type ShoppingListItem = {
+    name: string,
+    price: string,
+    quantity?: number,
+    imageSrc: string
 }
 
-type ShoppingListProps = {
-    children: ReactElement | ReactElement[]
+type ShoppingListCard = {
+    name: string,
+    lastUpdate: string,
+    products: ShoppingListItem[],
+    price: string
 }
 
-export const ShoppingListItem = ({  }: ShoppingListItemProps) => {
+type ShoppingList = {
+    children: null | React.ReactElement | React.ReactElement[]
+}
+
+export const ShoppingListCard = ({ name, lastUpdate, products, price }: ShoppingListCard) => {
     return (
-        <a href="" className="flex flex-col px-6 py-4 shadow-md rounded-lg mb-4">
-            <div className="">
-                <h1 className="text-lg font-medium">Akcesoria</h1>
-                <p>01 luty 2023 (ostatnia zmiana)</p>
+        <a href="" className="flex flex-col px-6 py-6 shadow-primary hover:shadow-secondary rounded-lg mb-6">
+            <div className="mb-4">
+                <h1 className="text-lg font-medium">{name}</h1>
+                <p className="text-gray-500">{lastUpdate} (ostatnia zmiana)</p>
             </div>
-            <div className="flex items-center justify-center">
-                <IconContext.Provider
-                    value={{
-                        style: {
-                            fontSize: '45px',
-                        }
-                    }}
-                >
+            <div className="flex items-center">
+                {products.map((product) =>
                     <div className="mx-4">
-                        <BsCardImage />
+                        <img src={product.imageSrc} alt="product" />
                     </div>
-                    <div className="mx-4">
-                        <BsCardImage />
-                    </div>
-                    <div className="mx-4">
-                        <BsCardImage />
-                    </div>
-                </IconContext.Provider>
+                )}
             </div>
             <div className="">
-                <p>199,00 zł</p>
+                <p className="mt-4">{price}</p>
             </div>
         </a>
     )
 }
 
-export const ShoppingList = ({ children }: ShoppingListProps) => {
+export const ShoppingListItem = ({ name, price, imageSrc }: ShoppingListItem) => {
     return (
-        <>
-            <div className="flex flex-row justify-between items-center mb-6">
+        <li className="flex flex-row justify-center items-center">
+            <input type="checkbox" className="" />
+            <img src={imageSrc} alt="" />
+            <div className="">
+                <a href="">{name}</a>
+            </div>
+            <div className="">
+                <p>{price}</p>
+            </div>
+            <div className="">
+                <select name="" id="">
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                    <option value="">4</option>
+                    <option value="">5</option>
+                </select>
+            </div>
+        </li>
+    )
+}
+
+export const ShoppingList = ({ children }: ShoppingList) => {
+    return (
+        <ul className="">
+            {/* <div className="flex flex-row justify-between items-center mb-6">
                 <h1 className="text-2xl">Listy zakupowe</h1>
                 <a href="" className="px-3 py-1 rounded hover:bg-slate-100">Zobacz więcej</a>
-            </div>
-            <div>
-                {children}
-            </div>
-        </>
+            </div> */}
+            {/* {items.map((item: ShoppingListItem) =>
+                <ShoppingListItem 
+                    name={item.name}
+                    price={item.price}
+                    quantity={item.quantity}
+                    imageSrc={item.imageSrc}
+                />
+            )} */}
+            {children}
+        </ul>
     )
 }
