1180bd9a219d9630fa0fd2d464a608bedf476597
mbugiel656 <mbugiel656@gmail.com>
2023-10-20 16:57:43 +0200

    Create slice to get the user account data

---
+++ src/features/account/accountSlice.tsx
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { useSelector } from "react-redux"
+import api from "../../api"
+import { RootState } from "../../app/store"
+
+
+interface AccountState {
+    status: string,
+    error: string | undefined,
+    account: {}
+}
+
+export const fetchAccountData = createAsyncThunk('account/fetchAccountData', async () => {
+    const response = await api.get('customers/user')
+    return response.data
+})
+
+const initialState: AccountState = {
+    status: 'idle',
+    error: undefined,
+    account: {}
+}
+
+export const accountSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder
+            .addCase(fetchAccountData.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchAccountData.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.account = action.payload
+            })
+            .addCase(fetchAccountData.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+    }
+})
+
+// export const {  } = accountSlice.actions
+
+export const selectStatus = (state: RootState) => state.account.status
+
+export const selectError = (state: RootState) => state.account.error
+
+export const selectAccount = (state: RootState) => state.account.account
+
+export default accountSlice.reducer
