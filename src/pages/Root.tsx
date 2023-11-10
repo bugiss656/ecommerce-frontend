fb2577e4dcb059617f228e37a70b85745da2c859
mbugiel656 <mbugiel656@gmail.com>
2023-11-10 11:48:52 +0100

    Add dropdown content to "Twoje konto" item based on user authentication status

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -7,16 +7,22 @@ import MenuItem from "../components/Menu/MenuItem"
 import Navigation from "../components/Navigation/Navigation"
 import NavigationList from "../components/Navigation/NavigationList"
 import NavigationItem from "../components/Navigation/NavigationItem"
+import Button from "../components/Button/Button"
 import { Link, Outlet } from "react-router-dom"
 
 import logo from "../media/logo.png"
 import { BsHeadset, BsPerson, BsCart3 } from "react-icons/bs"
+import { BsCardList } from 'react-icons/bs'
+import { FaRegAddressCard } from 'react-icons/fa'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { FiSettings } from 'react-icons/fi'
 
 import { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../app/hooks"
-import { selectAccount } from "../features/account/accountSlice"
+import { selectAccount, Status } from "../features/account/accountSlice"
 import { selectAccountStatus, selectAccountError, fetchAccountData } from "../features/account/accountSlice"
-import { selectLoginStatus, selectLoginError, handleUserLogout } from "../features/account/loginSlice"
+import { handleUserLogout } from "../features/account/loginSlice"
+import { AccountMenu, AccountMenuItem } from "../components/AccountMenu/AccountMenu"
 
 
 const Root = () => {
@@ -80,15 +86,77 @@ const Root = () => {
                             text="Twoje konto"
                             dropdown={true}
                             children={
-                                <ul>
-                                    <li>Twoje konto</li>
-                                    <li>Zamówienia</li>
-                                    <li>Ustawienia konta</li>
-                                    <li onClick={() => {
-                                        dispatch(handleUserLogout())
-                                        location.reload() }}>Wyloguj</li>
-                                </ul>
-                            }
+                                localStorage.getItem('authToken') ? (
+                                    <>
+                                        {accountStatus === Status.LOADING && <div>Loading...</div>}
+                                        {accountStatus === Status.FAILED && <div>{accountError}</div>}
+                                        {accountStatus === Status.SUCCEEDED && (
+                                            <>
+                                                <div className="p-3 mb-3">
+                                                    <p className="text-sm text-gray-500">Cześć,</p>
+                                                    <h1 className="text-xl">{account?.first_name}</h1>
+                                                </div>
+                                                <AccountMenu>
+                                                    <AccountMenuItem 
+                                                        icon={<BsCardList />} 
+                                                        text='Zamówienia' 
+                                                        href="/konto/zamowienia"
+                                                        className="flex flex-row items-center px-3 py-2 hover:cursor-pointer hover:bg-slate-100"
+                                                    />
+                                                    <AccountMenuItem 
+                                                        icon={<FaRegAddressCard />} 
+                                                        text='Dane do zamówień' 
+                                                        href="/konto/dane-do-zamowien"
+                                                        className="flex flex-row items-center px-3 py-2 hover:cursor-pointer hover:bg-slate-100"
+                                                    />
+                                                    <AccountMenuItem 
+                                                        icon={<AiOutlineHeart />} 
+                                                        text='Listy zakupowe' 
+                                                        href="/konto/listy-zakupowe"
+                                                        className="flex flex-row items-center px-3 py-2 hover:cursor-pointer hover:bg-slate-100"
+                                                    />
+                                                    <AccountMenuItem 
+                                                        icon={<FiSettings />} 
+                                                        text='Ustawienia konta' 
+                                                        href="/konto/ustawienia-konta"
+                                                        className="flex flex-row items-center px-3 py-2 hover:cursor-pointer hover:bg-slate-100"
+                                                    />
+                                                </AccountMenu>
+                                                <hr className="mt-3 mb-1" />
+                                                <div className="px-3 py-2">
+                                                    <Button 
+                                                        className="rounded-md text-sm w-full px-20 py-2 hover:bg-gray-200"
+                                                        text="Wyloguj się" 
+                                                        onClick={() => {
+                                                            dispatch(handleUserLogout())
+                                                            location.reload()
+                                                            console.log('clicked')
+                                                        }}   
+                                                    />
+                                                </div>
+                                                
+                                            </>
+                                        )}
+                                    </>
+                                ) : (
+                                    <div className="flex flex-col items-center w-full p-3">
+                                        <Button 
+                                            className="rounded-md text-white text-sm w-full px-20 py-2 bg-green-500 hover:bg-green-600"
+                                            text="Zaloguj się"
+                                            href="/logowanie"
+                                        />
+                                        <div className="flex flex-row justify-center items-center my-3">
+                                            <div className="h-[1px] w-14 bg-gray-300"></div>
+                                            <p className="text-sm text-gray-500 mx-2">Nie masz konta?</p>
+                                            <div className="h-[1px] w-14 bg-gray-300"></div>
+                                        </div>
+                                        <Button 
+                                            className="rounded-md border-[1px] text-blue-500 bg-white border-blue-500 text-sm px-20 py-2 hover:text-white hover:bg-blue-500"
+                                            text="Załóż konto"
+                                            href="/rejestracja"
+                                        />
+                                    </div>
+                                )}
                         />
                         <MenuItem
                             link="#"
