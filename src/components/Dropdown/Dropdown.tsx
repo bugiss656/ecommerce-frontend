a588751576c605fb62945b495eebbf429b86cf25
mbugiel656 <mbugiel656@gmail.com>
2023-11-10 11:54:38 +0100

    Refactoring in Dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -8,7 +8,7 @@ type DropdownProps = {
 
 const Dropdown = ({ children, isOpen }: DropdownProps) => {
     return (
-        <div className={`absolute ${isOpen ? 'block' : 'hidden'} right-0 min-w-[120px] bg-white shadow-[0_0_3px_0_rgba(0,0,0,0.2)] rounded-tl-md rounded-bl-md rounded-br-md p-3`}>
+        <div className={`absolute ${isOpen ? 'block' : 'hidden'} w-auto min-w-[120px] right-0 bg-white shadow-[0_0_3px_0_rgba(0,0,0,0.2)] rounded-tl-md rounded-bl-md rounded-br-md whitespace-nowrap`}>
             {children}
         </div>
     )
