5f7c524c8a36cf7e26a0d857047e1323cd510654
mbugiel656 <mbugiel656@gmail.com>
2023-11-13 16:41:05 +0100

    Refactoring Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom"
 
 
+type ButtonType = 'button' | 'submit' | 'reset' | undefined
+
 interface ButtonProps {
     className: string,
+    type?: ButtonType,
     href?: string,
     text: string,
     onClick?: () => void
 }
 
-const Button = ({ className, text, href, onClick }: ButtonProps) => {
+const Button = ({ className, type, text, href, onClick }: ButtonProps) => {
         if (href) {
             return (
                 <Link to={href} onClick={onClick}>
-                    <button className={className}>{text}</button>
+                    <button className={className} type={type}>{text}</button>
                 </Link>
             )
         } else {
             return (
-                <button className={className} onClick={onClick}>{text}</button>
+                <button className={className} type={type} onClick={onClick}>{text}</button>
             )
         }
 }
