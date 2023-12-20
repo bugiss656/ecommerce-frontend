d3e5aa637abd562619efbda9a7d1bd1eef02f6ba
mbugiel656 <mbugiel656@gmail.com>
2023-12-20 19:24:29 +0100

    Create ProductCard component | Update Products page

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+
+type ProductCardProps = {
+    image: string,
+    title: string,
+    href: string,
+    availability: number,
+    price: number
+}
+
+const ProductCard = ({ image, title, href, availability, price }: ProductCardProps) => {
+    return (
+        <Link to={href} className="flex flex-col items-center p-5 m-3 hover:shadow-secondary hover:rounded-md">
+            <div className="mb-5">
+                <img src={image} alt={title} />
+            </div>
+            <div className="items-left">
+                <h1 className="text-lg mb-2">{title}</h1>
+                {availability > 0 ? 
+                    <p className="text-green-600">Dostępny w magazynie</p> : 
+                    <p className="text-red-600">Produkt niedostępny</p>}
+                <p className="text-xl font-medium mt-3">{price} zł</p>
+            </div>
+        </Link>
+    )
+}
+
+export default ProductCard
---
+++ src/pages/Products.tsx
@@ -0,0 +1,46 @@
+import { useParams } from "react-router-dom"
+import { useAppDispatch, useAppSelector } from "../app/hooks"
+import {
+    selectProductsStatus,
+    selectProductsError,
+    selectProducts,
+    fetchProducts
+} from "../features/products/productsSlice"
+import { useEffect } from "react"
+import { convertStringToSlug } from "../utils/functions"
+import ProductCard from "../components/ProductCard/ProductCard"
+
+const Products = () => {
+    const { category } = useParams()
+    const dispatch = useAppDispatch()
+
+    const productsStatus = useAppSelector(selectProductsStatus)
+    const productsError = useAppSelector(selectProductsError)
+    const products = useAppSelector(selectProducts)
+
+    useEffect(() => {
+        dispatch(fetchProducts({ category: category }))
+    }, [dispatch, category])
+
+    return (
+        <div className="my-6">
+            <h1 className="text-3xl">{}</h1>
+            <div className="flex flex-row flex-wrap justify-center">
+                {products &&
+                    products.map((product) =>
+                        <ProductCard
+                            image={product.main_image}
+                            title={product.name}
+                            href={`/produkty/${convertStringToSlug(product.name)}`}
+                            availability={product.stock_quantity} 
+                            price={product.price}
+                        />
+                    )
+                    
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Products
