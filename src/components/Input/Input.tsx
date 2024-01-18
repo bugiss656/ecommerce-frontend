80c9dc1c1ca4dcb769156842b819b92c0f482c17
mbugiel656 <mbugiel656@gmail.com>
2024-01-18 10:41:18 +0100

    Refactoring Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,29 +1,48 @@
+import { UseFormRegister } from "react-hook-form"
 
 
-interface InputProps {
-    type: "button" | "email" | "password" | "submit" | "text" | undefined,
-    placeholder: string | undefined,
-    value: string | undefined,
-    onBlur?: () => void,
-    onChange?: () => void,
-    onClick?: () => void,
-    onFocus?: () => void
+type InputErrorProps = {
+    message: string | undefined
 }
 
+type InputProps = {
+    type: "button" | "email" | "password" | "submit" | "text" | "number",
+    name: string,
+    label?: string,
+    placeholder?: string,
+    errors: any,
+    register: UseFormRegister<any>
+}
 
-const Input = ({ type, placeholder, value, onBlur, onChange, onClick, onFocus }: InputProps) => {
+export const InputError = ({ message }: InputErrorProps) => {
     return (
-        <input 
-            className="border rounded w-full px-3 py-2 focus:outline-none focus:shadow-md"
-            role="input"
-            type={type} 
-            placeholder={placeholder}
-            value={value}
-            onBlur={onBlur}
-            onChange={onChange}
-            onClick={onClick}
-            onFocus={onFocus}
-        />
+        <span className="text-sm text-red-500">{message}</span>
+    )
+}
+
+const Input = ({ 
+    type, 
+    name, 
+    label, 
+    placeholder, 
+    register, 
+    errors, 
+    ...props 
+}: InputProps) => {
+    return (
+        <>
+            <label htmlFor={name}>{label}</label>
+            <input 
+                id={name}
+                className="border rounded w-full px-3 py-2 focus:outline-none focus:shadow-md"
+                type={type}
+                role="input"
+                placeholder={placeholder}
+                {...register(name)}
+                {...props}
+            />
+            {errors[name] ? <InputError message={errors[name].message} />  : null}
+        </>
     )
 }
 
