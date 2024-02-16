33b65e77cf119cfbdb2caa10d99598c1b7b24597
mbugiel656 <mbugiel656@gmail.com>
2024-02-16 16:00:21 +0100

    Update redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,17 +3,25 @@ import { PreloadedState } from "@reduxjs/toolkit"
 
 import loginReducer from "../features/account/loginSlice"
 import registerReducer from "../features/account/registerSlice"
+import updateAccountReducer from "../features/account/updateAccountSlice"
+import updateEmailReducer from "../features/account/updateEmailSlice"
+import updatePasswordReducer from "../features/account/updatePasswordSlice"
 import accountReducer from "../features/account/accountSlice"
 import bannerReducer from "../features/banner/bannerSlice"
 import categoriesReducer from "../features/categories/categoriesSlice"
 import productsReducer from "../features/products/productsSlice"
 import productDetailReducer from "../features/products/productDetailSlice"
+import checkPasswordReducer from "../features/account/checkPasswordSlice"
 
 
 const rootReducer = combineReducers({
     login: loginReducer,
     register: registerReducer,
     account: accountReducer,
+    updateAccount: updateAccountReducer,
+    updateEmail: updateEmailReducer,
+    updatePassword: updatePasswordReducer,
+    checkPassword: checkPasswordReducer,
     banner: bannerReducer,
     categories: categoriesReducer,
     products: productsReducer,
