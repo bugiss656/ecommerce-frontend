c9ffdcb0fe60e1f8252848216eefa3b5bb00d02f
mbugiel656 <mbugiel656@gmail.com>
2024-05-11 10:28:16 +0200

    Create Alert component

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,35 @@
+import { IoMdClose } from "react-icons/io"
+
+
+interface AlertProps {
+    type: 'success' | 'warning' | 'danger',
+    message: string,
+    onClick: () => void
+}
+
+const Alert = ({ type, message, onClick }: AlertProps) => {
+    let alertColor = ''
+
+    switch (type) {
+        case 'success':
+            alertColor = 'bg-green-200'
+            break
+        case 'warning':
+            alertColor = 'bg-yellow-200'
+            break
+        case 'danger':
+            alertColor = 'bg-red-200'
+            break
+    }
+
+    return (
+        <div className={`rounded-md my-3 p-3 ${alertColor}`} role="alert">
+            <div className="flex flex-row justify-between items-center">
+                <div className="mx-3">{message}</div>
+                <IoMdClose className="text-2xl hover:cursor-pointer" onClick={onClick} />
+            </div>
+        </div>
+    )
+}
+
+export default Alert
